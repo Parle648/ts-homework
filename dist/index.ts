7ec9b3b8ee8d22e2f2362e56b71e7872c1a9a054
Parle648 <sashahoumi@gmail.com>
2024-10-26 15:23:44 +0300

    + Solved task

---
+++ dist/index.ts
@@ -0,0 +1,7 @@
+const x: number = 123
+
+function logNumber(param: number): undefined {
+  console.log(param)
+}
+
+logNumber(x)
---
+++ web/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const x = 123;
+function logNumber(param) {
+    console.log(param);
+}
+logNumber(x);
